ee34d0430f79f709cea79928fc440dcc98ec4484
Jooahru <dndlf333@naver.com>
2021-04-27 14:50:34 +0900

    firstcommit

---
+++ fortest/src/main/java/com/multi/fortest/FortestApplication.java
@@ -0,0 +1,13 @@
+package com.multi.fortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortestApplication.class, args);
+	}
+
+}
---
+++ fortest/src/main/java/com/multi/fortest/HelloController.java
@@ -0,0 +1,13 @@
+package com.multi.fortest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	@RequestMapping("/hello")
+	public String start()
+	{
+		return "hello";
+	}
+}
